b8abd53c46c030609cc9c9a20a5210795751a636
Wali-Muhammad <luckywali0986@gmail.com>
2026-06-26 17:32:24 +0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         credentials: true
     end
     allow do
-        origins 'https://supercarsrentals.onrender.com'
+        origins 'https://rental-rkib.onrender.com'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
